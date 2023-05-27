c21d30835087b251591474e093d531cdbabd0e06
Alberth Cruz Matos <alberth@matos.cc>
2023-05-26 21:10:59 -0400

    Week 1 assignments

---
+++ MatosAlberth_modification_wk1.py
@@ -0,0 +1,13 @@
+print('Hello Class!')
+print('My name is Nathan Painter')
+print("CMIS 102 6381")
+print("18MAY2023")
+myname="Nathan"
+myClass="CMIS 102 6381"
+print('My name is:\t', myname)
+print('My class is', myClass)
+
+myName = "Alberth Matos"
+myClass = "CMIS 102 6381"
+print("Modified name: ", myName)
+print("Modified class: ", myClass)
---
+++ MatosAlberth_wk1.py
@@ -0,0 +1,12 @@
+# Alberth Matos, CMIS 102/6381, 18 May 2023
+
+print("Hello, class!  Alberth Matos, CMIS 102-6381, May 5 2023")
+
+myName = "Alberth Matos"
+className = "CMIS 102-6381"
+
+print(myName)
+print(className)
+
+print("My name is: \t ", myName)
+print("My class is: \t", className)
